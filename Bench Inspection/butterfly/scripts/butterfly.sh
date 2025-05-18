1168b75b68d40ccfc10d15dd5e6d7130840a0dbb
Gideon Tladi <117656101+gtcodes22@users.noreply.github.com>
2025-05-18 20:31:56 +0100

    Delete Bench Inspection/butterfly directory
    Had previously uploaded a test version of the code here, instead of the actual code displayed by the team during bench inspection.

--- Bench Inspection/butterfly/scripts/butterfly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Terminal 1 - Runs butterfly workspace  build setup, and launches the drone simulation in Gazebo Harmonic.
-gnome-terminal -- bash -c "cd ~/butterfly && \
-colcon build && \
-source ~/butterfly/install/setup.bash && \
-ros2 launch butterfly launch.py &&\
-echo 'butterfly simulation set up successful!' && exec bash"
